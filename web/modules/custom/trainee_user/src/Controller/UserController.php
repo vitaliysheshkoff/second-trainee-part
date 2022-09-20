c3c61d57965521e518cdc3affe2c9f607da3105a
vitaliysheshkoff <vitaliysheshkoff@gmail.com>
2022-09-20 13:26:00 +0300

    cleaned up code

--- web/modules/custom/trainee_user/src/Controller/UserController.php
+++ web/modules/custom/trainee_user/src/Controller/UserController.php
@@ -8,7 +8,7 @@ use Drupal\Core\Controller\ControllerBase;
 /**
  * Provides route for trainee user module
  */
-class UserController extends ControllerBase{
+class UserController extends ControllerBase {
   public function users(): array{
     $data = Drupal::service('trainee_user.user_manager')->getList(20)[0]->name;
     return [
