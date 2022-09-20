d2d446a54587fe95fb91f8dd057b7fa7e1f41c96
vitaliysheshkoff <vitaliysheshkoff@gmail.com>
2022-09-20 12:50:46 +0300

    cleaned up code

--- web/modules/custom/empty_module/src/Controller/HelloController.php
+++ web/modules/custom/empty_module/src/Controller/HelloController.php
@@ -1,41 +1,16 @@
 <?php
 
-/**
- * @file
- * Contains \Drupal\empty_module\Controller\HelloWorld.
- */
-
-namespace Drupal\empty_module\Controller;
+namespace Drupal\trainee_empty_module\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 
 /**
- * Provides route for empty module
+ * Provides route for trainee empty module
  */
-
 class HelloController extends ControllerBase {
-
-  public function hello(): array
-  {
-
-   // $data = \Drupal::service('trainee_user.user_manager')->updateUser(7252, 'Has Fs','male','654321123456pavlo@mail.com', 'inactive');
-   // $data = \Drupal::service('trainee_user.user_manager')->getUsers(1); /*7252*/
-    $data = \Drupal::service('trainee_user.user_manager')->deleteUser(7252); /*7252*/
-
+  public function hello(): array {
     return [
-      '#markup' => "Hello world {$data}"
-    ];
-  }
-
-  public function users(): array
-  {
-
-    // $data = \Drupal::service('trainee_user.user_manager')->updateUser(7252, 'Has Fs','male','654321123456pavlo@mail.com', 'inactive');
-    $data = \Drupal::service('trainee_user.user_manager')->getUsers(1); /*7252*/
-   // $data = \Drupal::service('trainee_user.user_manager')->deleteUser(7252); /*7252*/
-
-    return [
-      '#markup' => "Hello world {$data}"
+      '#markup' => "Hello world!"
     ];
   }
 }
