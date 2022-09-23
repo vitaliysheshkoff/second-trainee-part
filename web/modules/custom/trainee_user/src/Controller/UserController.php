845a89efdf39e6ec970f011fcb44be57fa2542a6
vitaliysheshkoff <vitaliysheshkoff@gmail.com>
2022-09-23 09:47:04 +0300

    disabled cache

--- web/modules/custom/trainee_user/src/Controller/UserController.php
+++ web/modules/custom/trainee_user/src/Controller/UserController.php
@@ -10,10 +10,10 @@ use Drupal\Core\Controller\ControllerBase;
  */
 class UserController extends ControllerBase {
   public function showUserList(int $page = 1): array {
+    Drupal::service('page_cache_kill_switch')->trigger();
     $userList = Drupal::service('trainee_user.user_manager_service')->getList($page);
     return [
       '#theme' => 'trainee_user_list',
-      '#attached' => ['library' => ['trainee_user/form']],
       '#user' => $userList,
     ];
   }
