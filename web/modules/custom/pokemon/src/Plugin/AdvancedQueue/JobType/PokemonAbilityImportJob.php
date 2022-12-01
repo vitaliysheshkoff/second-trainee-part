1c1136ebe298ed52bc9d14e594a06838ad3564d6
Pavel Vashchyla <empty.12.95@gmail.com>
2022-12-01 11:21:49 +0100

    Task 18: Added small changes

--- web/modules/custom/pokemon/src/Plugin/AdvancedQueue/JobType/PokemonAbilityImportJob.php
+++ web/modules/custom/pokemon/src/Plugin/AdvancedQueue/JobType/PokemonAbilityImportJob.php
@@ -17,14 +17,11 @@ class PokemonAbilityImportJob extends PokemonBaseJobType {
    * {@inheritdoc}
    */
   public function process(Job $job) {
-    /** @var \Drupal\pokemon\PokemonManager $pokemon_manager */
-    $pokemon_manager = \Drupal::service('pokemon.pokemon_manager');
-
     $payload = $job->getPayload();
     // Как только мы получили пэйлоад мы должны вызвать менеджера покемонов
     // В этом менеджере мы должны вызвать метод который по определенному эндпоинту вернет нам результат.
     // Результат мы запихнем в метод создания таксономии
-    $pokemon_manager->createTaxonomy($payload['endpoint'], $payload['description'], $payload['list']);
+    $this->pokemonManager->createTaxonomy($payload['endpoint'], $payload['description'], $payload['list']);
     // Метод по созданию таксономии нам тоже должен что-то возвращать
     // И на основании этого результата мы уже будет возвращать
     // return JobResult(и сюда уже нужно передавать необходимые параметры посмотри уже этот метод сам);
