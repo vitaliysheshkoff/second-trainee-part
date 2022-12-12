e4c725f99dc208642cd10216b8dcc0138f7d5a86
vitaliysheshkoff <vitaliysheshkoff@gmail.com>
2022-12-12 09:31:47 +0100

    added condition

--- web/modules/custom/pokemon/src/Controller/PokemonNodeController.php
+++ web/modules/custom/pokemon/src/Controller/PokemonNodeController.php
@@ -10,15 +10,35 @@ use Drupal\taxonomy\TermInterface;
  * Class PokemonNodeController.
  */
 class PokemonNodeController extends ControllerBase {
+
+  const POKEMONS_FIELDS = [
+    'field_ability',
+    'field_color',
+    'field_egg_group',
+    'field_form',
+    'field_habitat',
+    'field_height',
+    'field_id',
+    'field_name',
+    'field_pokemon_image',
+    'field_shape',
+    'field_specie',
+    'field_stat',
+    'field_type',
+    'af'
+  ];
+
   /**
    * {@inheritdoc}
    */
   public function render(Node $node) {
+    if ($node->bundle() === 'pokemon') {
       return [
         '#theme' => 'pokemon_node',
         '#node' => $node,
         '#attached' => ['library' => ['pokemon/node-style']],
       ];
     }
+  }
 
 }
